a8d4fcb236d5e4b632e9e3bc4a93f050ed5d677d
Sonjaemin95 <hgh670604@gmail.com>
2023-03-07 15:36:44 +0900

    에러 수정

--- extensions/dicom-segmentation/src/components/SegmentationPanel/adapters/Cornerstone/Segmentation_4X.js
+++ extensions/dicom-segmentation/src/components/SegmentationPanel/adapters/Cornerstone/Segmentation_4X.js
@@ -102,7 +102,7 @@ function fillSegmentation(segmentation, inputLabelmaps3D, userOptions = {}) {
     const referencedFramesPerSegment = [];
 
     //for (let i = 1; i < metadata.length; i++) {
-    for (let i = 1; i < labelmaps3D[0].activeSegmentIndex+1; i++) {
+    for (let i = 1; i < labelmaps3D[0].labelmaps2D[0].segmentsOnLabelmap.length; i++) {
       console.log(metadata[i]);
       if (metadata[i]) {
         referencedFramesPerSegment[i] = [];
