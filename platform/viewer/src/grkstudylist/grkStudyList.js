38849f5ecd7dbbbf0240ec11151a3a935f18c078
ss <winop7@gmail.com>
2023-06-30 14:02:11 +0900

    hook으로 변경

--- platform/viewer/src/grkstudylist/grkStudyList.js
+++ platform/viewer/src/grkstudylist/grkStudyList.js
@@ -1,138 +1,117 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { ViewerbaseDragDropContext } from '@ohif/ui';
 import { withRouter } from 'react-router';
 // import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-
 // const navigate = useNavigate();   /////////////////////////////
-class ProjectList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      token: '',
-      tokenData: {},
-    };
-  }
+const Login = () => {
+  const [studyObj, setStudyObj] = useState([]);
 
-  goStudy = (oid) => {
-    this.navigate(`/${oid}/subjectlist`)
-    console.log(oid, "스터디 목록 눌림");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const goStudy = oid => {
+    this.navigate(`/${oid}/subjectlist`);
+    console.log(oid, '스터디 목록 눌림');
   };
 
-  // get_study_list = () =>{
-  //   const accessTokenPotal = localStorage.getItem('accessTokenPotal');
-  //   const refreshToken = localStorage.getItem('refreshToken');
-  // }
-
-  fetchData = async () => {
+  const fetchData = async () => {
     try {
-      const accessTokenPotal = JSON.parse(localStorage.getItem("accessTokenPotal"));
-      const response = await axios.get('http://grk-backend.medical-lab.co.kr/api/v1/study', {
-        headers: {
-          'Authorization': `Bearer ${accessTokenPotal.token}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      return response;
+      const accessTokenPotal = JSON.parse(
+        localStorage.getItem('accessTokenPotal')
+      );
+      const response = await axios.get(
+        'http://grk-backend.medical-lab.co.kr/api/v1/study',
+        {
+          headers: {
+            Authorization: `Bearer ${accessTokenPotal.token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      setStudyObj(response.data.data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  async render() {
-    // const { token, tokenData } = this.state;
-    let tokenData = {}
-    try {
-      const accessTokenPotal = JSON.parse(localStorage.getItem("accessTokenPotal"));
-      const response = await axios.get('http://grk-backend.medical-lab.co.kr/api/v1/study', {
-        headers: {
-          'Authorization': `Bearer ${accessTokenPotal.token}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      tokenData = response;
-    } catch (error) {
-      console.error(error);
-    }
-    let studyObj = tokenData.data.data;
-    console.log("render start=======");
-    console.log(studyObj)
-    return (
-      // (!token)?
-      // // this.navigate('/login'):
-      // console.log("!!!!!!!!!!!!!!!!!!no token"):
-      (
-      <section className="bg-black  md:h-screen">
-        <div className="flex flex-row items-center bg-black border-b border-gray-900 z-20 sticky top-0 justify-between">
-          <div className="flex justify-between flex-1  container py-4  m-auto">
-            <div className="flex items-center">
-              <div className="inline-flex items-center mr-3">
-                <div className="ml-4">
-                  {/* <Svg
+  return (
+    // (!token)?
+    // // this.navigate('/login'):
+    // console.log("!!!!!!!!!!!!!!!!!!no token"):
+    <section className="bg-black  md:h-screen">
+      <div className="flex flex-row items-center bg-black border-b border-gray-900 z-20 sticky top-0 justify-between">
+        <div className="flex justify-between flex-1  container py-4  m-auto">
+          <div className="flex items-center">
+            <div className="inline-flex items-center mr-3">
+              <div className="ml-4">
+                {/* <Svg
                     name="logo-ohif"
                     style={{ width: '180px', height: '40px' }}
                   /> */}
-                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="container relative flex flex-col m-auto">
-          <div className="mt-10 py-5 border-b border-gray-900">
-            <h3 className="text-white text-bold text-2xl">Study List</h3>
-            <p className="text-white text-bold text-base mt-2">
-            {studyObj.length} <span className="text-primary-light ml-2">Studys</span>
-            </p>
-          </div>
-          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8 mt-5">
-            <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="overflow-hidden rounded-lg">
-                <table className="min-w-full text-center ">
-                  <thead className="border-b bg-gray-900 border-primary-light">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="text-sm font-medium text-white px-6 py-4"
-                      >
-                        #
-                      </th>
-                      <th
-                        scope="col"
-                        className="text-sm font-medium text-white px-6 py-4 text-left"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="text-sm font-medium text-white px-6 py-4"
-                      >
-                        Description
-                      </th>
-                      <th
-                        scope="col"
-                        className="text-sm font-medium text-white px-6 py-4"
-                      >
-                        Start
-                      </th>
-                      <th
-                        scope="col"
-                        className="text-sm font-medium text-white px-6 py-4"
-                      >
-                        End
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {studyObj.map((row, index) => (
-                      <tr
-                      key = {index}
+      </div>
+      <div className="container relative flex flex-col m-auto">
+        <div className="mt-10 py-5 border-b border-gray-900">
+          <h3 className="text-white text-bold text-2xl">Study List</h3>
+          <p className="text-white text-bold text-base mt-2">
+            {studyObj.length}{' '}
+            <span className="text-primary-light ml-2">Studys</span>
+          </p>
+        </div>
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8 mt-5">
+          <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden rounded-lg">
+              <table className="min-w-full text-center ">
+                <thead className="border-b bg-gray-900 border-primary-light">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      #
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4 text-left"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Description
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Start
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-white px-6 py-4"
+                    >
+                      End
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {studyObj.map((row, index) => (
+                    <tr
+                      key={index}
                       className="bg-gray-800 cursor-pointer hover:bg-secondary-main"
                       onClick={() => this.goStudy(row.oid)}
                     >
                       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-100">
-                        {index+1}
+                        {index + 1}
                       </td>
                       <td className="text-sm text-gray-100 font-light px-6 py-4 whitespace-nowrap text-left">
                         {row.studyName}
@@ -147,30 +126,15 @@ class ProjectList extends Component {
                         {row.endDate}
                       </td>
                     </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </section>)
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    user: state.oidc.user,
-  };
+      </div>
+    </section>
+  );
 };
 
-
-const ConnectedLogin = connect(
-  mapStateToProps,
-  null
-)(ProjectList);
-
-export default ViewerbaseDragDropContext(
-  withRouter(ConnectedLogin)
-);
+export default Login;
