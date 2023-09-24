6d0a668135b7f962767aa114bf9a8998f7c877ef
ss <winop7@gmail.com>
2023-09-24 21:08:42 +0900

    데이터 문제 처리

--- platform/core/src/DICOMWeb/getModalities.js
+++ platform/core/src/DICOMWeb/getModalities.js
@@ -12,6 +12,10 @@ export default function getModalities(Modality, ModalitiesInStudy) {
     if (modalities.vr && modalities.vr === ModalitiesInStudy.vr) {
       for (let i = 0; i < ModalitiesInStudy.Value.length; i++) {
         const value = ModalitiesInStudy.Value[i];
+        console.log(modalities.Value, value, Modality);
+        if (modalities.Value == undefined) {
+          modalities.Value = [];
+        }
         if (modalities.Value.indexOf(value) === -1) {
           modalities.Value.push(value);
         }
